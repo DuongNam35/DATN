87da82d75396364236ba125c0d12981ccebd1b80
= <=>
2022-05-07 15:42:21 +0700

    add action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,37 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from datetime import datetime
+
+class ActionGaveTime(Action):
+
+    def name(self) -> Text:
+        return "action_give_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=f"{datetime.today()}")
+
+        return []
+
+# class ActionSayUserName(Action):
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_say_user_name"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         user_name = tracker.get_slot("user_name")
+#         if not user_name:
+#             dispatcher.utter_message(text="I don't know your name.")
+#         else:
+#             dispatcher.utter_message(text=f"Your name is {user_name}!")
 #         return []
